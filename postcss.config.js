e5d6d519851e9e27dec6bd708b42c5751759d2ae
旋涡鸣人 <2264371622@qq.com>
2020-04-24 10:50:46 +0800

    提交

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    plugins:[
+    plugins: [
         require('postcss-import'),
         require('postcss-cssnext'),
-        require('autoprefixer'),
         require('cssnano')
     ],
 }
